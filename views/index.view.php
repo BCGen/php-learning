a3504681c2bb6a6611b0599c28d6abe29ff345bc
Paul Chen <bcgenever7@gmail.com>
2019-08-23 22:46:06 +0800

    first commit

---
+++ views/index.view.php
@@ -0,0 +1,10 @@
+<?php require('partials/head.php'); ?>
+
+  <h1>My Tasks</h1>
+
+  <form action="/names" method="POST">
+    <input name="name">
+    <button type="submit">Submit</button>
+  </form> 
+
+<? require('partials/footer.php'); ?>
